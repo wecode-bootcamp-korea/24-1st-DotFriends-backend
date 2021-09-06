abfb3cdf5939cdfa48f99e16da1046ca19904e94
sdk <sdk1926@naver.com>
2021-09-06 09:41:35 +0900

    [서동규]Add: 상품상세페이지 API 구현

--- products/views.py
+++ products/views.py
@@ -3,9 +3,10 @@ import json
 
 from django.http      import JsonResponse
 from django.views     import View
-from django.db.models import Q
+from django.db.models import Q, Count
 
 from .models          import Product
+from comments.models  import Comment
 from .decorator       import input_validator
 
 class ProductsView(View):
@@ -33,3 +34,28 @@ class ProductsView(View):
         }for product in products]
 
         return JsonResponse({'results': results}, status=200)  
+
+class ProductDetailView(View):
+    def get(self, request, product_id):
+        
+        if not (Product.objects.filter(id=product_id).exists()):
+            return JsonResponse({'MESSAGE': 'NOT_FOUND'}, status=404)
+
+        product = Product.objects.annotate(likes=Count("userproductlike")).get(id=product_id)
+        
+        results = {
+            'id':product.id,
+            'name':product.name,
+            'price':int(product.price),
+            'like': product.likes,
+            'images':[image.url for image in product.image_set.all()],         
+            }
+        reviews= [{
+            "user_name":comment.user.name,
+            "rate": int(comment.rate),
+            "text": comment.text,
+            "created_at":comment.created_at.date(),
+            "images":[image.url for image in comment.commentimage_set.all()]
+            }for comment in product.comment_set.all()]    
+        
+        return JsonResponse({'results': results, 'reviews':reviews}, status=200)
