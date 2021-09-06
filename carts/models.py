8be6b1e8d08afd169e04058cfaa06956bbfb63ac
신우주 <newspace@sin-ujuui-MacBookPro.local>
2021-09-06 18:43:06 +0900

    ADD/carts

--- carts/models.py
+++ carts/models.py
@@ -8,7 +8,7 @@ class Cart(models.Model):
     product       = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='carts')
     quantity      = models.IntegerField(default=0)
     created_at    = models.DateTimeField(auto_now_add=True)
-    updated_at    = models.DateTimeField(auto_now_add=True)
+    updated_at    = models.DateTimeField(auto_now=True)
 
 class Meta:
     db_table = 'carts'
