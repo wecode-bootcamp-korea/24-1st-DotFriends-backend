1028af10380202233f3df54bcef3b26f618e6597
Jack Kim <hion8800@gmail.com>
2021-09-07 16:07:57 +0900

    UPDATE : VISITOR_DECORATOR

--- products/decorator.py
+++ products/decorator.py
@@ -1,5 +1,6 @@
 from products.models import Category
 from django.http.response import HttpResponse
+import jwt
 
 def input_validator(func):
     def wraper(self, request, *args, **kwargs):
@@ -14,4 +15,19 @@ def input_validator(func):
         return func(self, request, *args, **kwargs)
     return wraper
 
-    
+def visitor_validator(func):
+    def wraper(self, request, *args, **kwargs):
+        try:
+            access_token = request.headers.get('Authorization',None)
+            if not access_token:
+                request.user = None
+                return func(self, request, *args, **kwargs)
+            token = jwt.decode(access_token, SECRET_KEY, algorithms='HS256')
+            user = User.objects.get(id=token['id'])
+            request.user = user
+        except User.DoesNotExist:
+            return JsonResponse({'MESSAGE':'INVALID_USER'},status=401)
+        except jwt.exceptions.DecodeError:
+            return JsonResponse({'MESSAGE':'INVALID_TOKEN'},status=401)
+        return func(self, request, *args, **kwargs)
+    return wraper
